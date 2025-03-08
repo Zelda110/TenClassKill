54e48d443bcf5280ea55612cfbc3d669ac7bd6be
ハイラル・ゼルダ <Zelda_110@icloud.com>
2025-03-09 07:39:35 +0800

    Initial Commit

---
+++ TenClassKill/TenClassKillApp.swift
@@ -0,0 +1,17 @@
+//
+//  TenClassKillApp.swift
+//  TenClassKill
+//
+//  Created by ハイラル・ゼルダ on 2025.03.09.
+//
+
+import SwiftUI
+
+@main
+struct TenClassKillApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
